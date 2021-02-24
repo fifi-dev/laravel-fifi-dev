1e7e2c77d803febd2aed1cda54d7e0ba881e6935
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-02-25 00:05:53 +0100

    _

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -98,6 +98,7 @@
                                     @endif
                                 @else
                                 <li class="nav-item"><a class="nav-link" href="{{route('login')}}"> <i class="fas fa-user-alt mr-1 text-gray"></i>Connexion</a></li>
+                                <li class="nav-item"><a class="nav-link" href="{{route('register')}}"> <i class="fas fa-user-alt mr-1 text-gray"></i>Inscription</a></li>
                                 @endif
 
                             @endif
