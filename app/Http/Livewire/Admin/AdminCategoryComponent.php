1066835cd30ce2119c8cc4f3a5eb88ba36f13caa
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-02-24 23:53:51 +0100

    delete_category_finished

--- app/Http/Livewire/Admin/AdminCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -9,6 +9,14 @@ use Livewire\WithPagination;
 class AdminCategoryComponent extends Component
 {
     use WithPagination;
+
+    public function deleteCategory($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('message','La catégorie a été suprimée !');
+    }
+
     public function render()
     {
         $categories = Category::paginate(6);
